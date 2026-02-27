3d5ca08a83b4b3235c6cb9adc61d45029491bff3
ferreiragoncalveskayke-del <ferreiragoncalveskayke@gmail.com>
2026-02-27 13:49:02 -0300

    aula 27/2

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,9 @@ function Home() {
   return (
     <div>
       <h1>React com Next JS</h1>
+      <h2>aula de introdução</h2>
+      <h3>leiaute e estilização</h3>
+      <p>nesta aula iremos entender o uso de css global e local.</p>
     </div>
   );
 }
