076f68c4af46dc0d039ab94d09c35bc46ef77f82
ferreiragoncalveskayke-del <ferreiragoncalveskayke@gmail.com>
2026-02-27 11:05:30 -0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -8,3 +8,26 @@ export default function Home() {
     </div>
   );
 }
+import Image from "next/image";
+import styles from "./page.module.css";
+
+const Card = () => {
+  return (
+    <div>
+      <h3>Aula introdução</h3>
+      <h3>Aula Estilização</h3>
+      <h3>Aula Componentes</h3>
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h1>React com Next JS</h1>
+    </div>
+  );
+}
+
+
+export default Home2;
