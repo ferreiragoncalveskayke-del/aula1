10dc24851b02c7d859ed422921f8fe6649a433b8
ferreiragoncalveskayke-del <ferreiragoncalveskayke@gmail.com>
2026-03-13 13:49:44 -0300

    13/03/26

---
+++ src/app/atividades/atv-01/page.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+import styles from "./page.module.css";
+function Atividade01 () {
+    return(
+        <div>
+            <h1>Atividade 1 </h1>
+            <Link href="/Exemplos/ex-01">Atividade01</Link>
+            <p>Atividade 1</p>
+        </div>
+    )
+}
+
+export default Atividade01;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,24 @@
-import Image from "next/image";
+import Link from 'next/link';
+
 import styles from "./page.module.css";
 
 function Home() {
   return (
      <div className={styles.containerHome}>
+
     <div className={styles.containerListas}>
+
       <h1>Exmplos</h1>
+      <Link href="/Exemplos/ex-01">Exemplo 1</Link>
+      <Link href="/Exemplos/ex-02">Exemplo 2</Link>
+
     </div>
+
     <div className={styles.containerListas}>
+
       <h1>Atividades</h1>
     </div>
+    
     </div>
   );
 }
